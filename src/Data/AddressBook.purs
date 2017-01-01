94336144f19f6014b61151cf4b02bdfcbe5a7de9
Curt J. Sampson <cjs@cynic.net>
2017-01-01 18:19:44 +0900

    Data.Addressbook: Up to section 3.5

---
+++ src/Data/AddressBook.purs
@@ -0,0 +1,43 @@
+module Data.AddressBook where
+
+import Prelude
+import Control.Plus (empty)
+import Data.List (List(Cons), filter, head)
+import Data.Maybe (Maybe)
+
+
+type AddressBook = List Entry
+
+type Entry =
+    { firstName :: String
+    , lastName  :: String
+    , address   :: Address
+    }
+
+type Address =
+    { street    :: String
+    , city      :: String
+    , state     :: String
+    }
+
+showEntry :: Entry -> String
+showEntry e = e.lastName
+    <> ", " <> e.firstName
+    <> ": " <> showAddress e.address
+
+
+showAddress :: Address -> String
+showAddress a = a.street <> ", " <> a.city <> ", " <> a.state
+
+emptyBook :: AddressBook
+emptyBook = empty
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry = Cons
+
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry fname lname book = head $ filter filterEntry book
+    where
+        filterEntry :: Entry -> Boolean
+        filterEntry e = e.firstName == fname && e.lastName == lname
+
