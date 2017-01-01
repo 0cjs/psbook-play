8c3d4f41a5806f85f78a718ba6105d7d680777b4
Curt J. Sampson <cjs@cynic.net>
2017-01-01 22:43:36 +0900

    Data.AddressBook: Comment on attempt to make Address an instance of Show

--- src/Data/AddressBook.purs
+++ src/Data/AddressBook.purs
@@ -20,14 +20,22 @@ type Address =
     , state     :: String
     }
 
+{- This would be nice to make work, but right now gives us an
+   InvalidInstanceHead error. Not sure how to handle it yet.
+instance showAddressInstance
+    :: Show { street::String, city::String, state::String }
+    where
+        show = showAddress
+-}
+
 showEntry :: Entry -> String
 showEntry e = e.lastName
     <> ", " <> e.firstName
     <> ": " <> showAddress e.address
 
-
 showAddress :: Address -> String
 showAddress a = a.street <> ", " <> a.city <> ", " <> a.state
+
 
 emptyBook :: AddressBook
 emptyBook = empty
